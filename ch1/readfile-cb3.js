1a796729e21838133cde9ebc1990b372ed271681
kamiokan <spherenov@gmail.com>
2022-04-17 23:55:13 +0900

    コールバック地獄の例

---
+++ ch1/readfile-cb3.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+// コールバック地獄(callback hell)
+fs.readFile('a.txt', 'utf-8', function(err, data) {
+  console.log('a.txtを読みました', data)
+  fs.readFile('b.txt', 'utf-8', function(err, data) {
+    console.log('b.txtを読みました', data)
+    fs.readFile('c.txt', 'utf-8', function(err, data) {
+      console.log('c.txtを読みました', data)
+    })
+  })
+})
